a76ac8e62fe00311c2c8168051626ed7a8c30860
Owen Osborn <owen@critterandguitari.com>
2018-04-16 07:18:06 -0400

    removing 3.0 update

--- UpdateOS-3.0/scripts/start-ap.sh
+++
@@ -1,3 +0,0 @@
-killall wpa_supplicant
-killall dhcpcd
-/root/scripts/create_ap --daemon -n wlan0 Organelle coolmusic
--- UpdateOS-3.0/web/server/helpers.py
+++
@@ -1,8 +0,0 @@
-import os
-
-def get_immediate_subdirectories(dir):
-    if os.path.isdir(dir):
-        return [name for name in os.listdir(dir) if os.path.isdir(os.path.join(dir, name))]  
-    else :
-        return []
-
